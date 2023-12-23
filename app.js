62a26c5c29e92cda7f28eb6c0b0c8e31c63b48c8
jaeseongDev <qkrwotjd1445@naver.com>
2023-12-23 23:33:42 +0900

    refactor: 불필요한 코드 삭제

--- app.js
+++ app.js
@@ -3,18 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', async (req, res) => {
-  const response = await axios.put('http://169.254.169.254/latest/api/token', null, {
-    headers: {
-      'X-aws-ec2-metadata-token-ttl-seconds': 21600
-    }
-  });
-  const token = response.data;
-  const metaDataResponse = await axios.get('http://169.254.169.254/latest/meta-data/', {
-    headers: {
-      'X-aws-ec2-metadata-token': token
-    }
-  });
-  res.send(metaDataResponse.data);
+  res.send("Hello World!");
 })
 
 app.get('/health', (req, res) => {
