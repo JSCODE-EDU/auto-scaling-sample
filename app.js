2d66bcd6a1de42580c6e8ecee3fc7fb40e6acbca
jaeseongDev <qkrwotjd1445@naver.com>
2023-12-23 23:14:28 +0900

    feat: 예시 코드 수정

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 80;
-const metadata = require('node-ec2-metadata');
 
-app.get('/', (req, res) => {
-  metadata.getMetadataForInstance('instance-id')
-    .then((instanceId) => {
-      res.send("Instance ID: " + instanceId);
-    })
-    .fail((error) => {
-      res.send(error)
-    });
+app.get('/', async (req, res) => {
+  const response = await axios.put('http://169.254.169.254/latest/api/token', null, {
+    headers: {
+      'X-aws-ec2-metadata-token-ttl-seconds': 21600
+    }
+  });
+  const token = response.data;
+  const metaDataResponse = await axios.get('http://169.254.169.254/latest/meta-data/', {
+    headers: {
+      'X-aws-ec2-metadata-token': token
+    }
+  });
+  res.send(metaDataResponse.data);
 })
 
 app.get('/health', (req, res) => {
